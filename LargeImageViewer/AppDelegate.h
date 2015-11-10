0c2dd80ecfe23926aec3ff0732145d909c99e54b
Adam <AdamBCo@me.com>
2015-11-09 20:41:02 -0700

    Initial Commit

---
+++ LargeImageViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LargeImageViewer
+//
+//  Created by Adam Cooper on 11/9/15.
+//  Copyright © 2015 Adam Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LargeImageViewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LargeImageViewer
+//
+//  Created by Adam Cooper on 11/9/15.
+//  Copyright © 2015 Adam Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
